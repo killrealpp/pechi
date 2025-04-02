4ab71b56fb0ec0f300ff76c03ac640fe72662b42
killrealpp <kaysarovkirill5@gmail.com>
2025-04-02 12:50:23 +0300

    add adaptiv

--- js/modules/swiper-catalog.js
+++ js/modules/swiper-catalog.js
@@ -1,20 +1,49 @@
+let swiperInstance = null
+
+function initializeSwiper(){
+    if (swiperInstance) return
+
+    swiperInstance = new Swiper('.swiper', {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        loop: true,
+        pagination:{
+            el: '.swiper-pagination',
+            clickable: true
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        }
+    })
+}
+
+function destroySwiper(){
+    if (swiperInstance){
+        swiperInstance.destroy(true, true)
+        swiperInstance = null
+    }
+}
+
 export function onWidthMatch(callback) {
-    const mediaQuery = window.matchMedia("(max-width: 700px)");
+    const mediaQuery = window.matchMedia("(max-width: 375px)");
 
     function checkSize(event) {
         if (event.matches) {
-            callback();
+            callback(true); // Когда ширина экрана <= 375px, вызываем слайдер
+        } else {
+            callback(false); // Когда ширина больше 375px, уничтожаем слайдер
         }
     }
 
     mediaQuery.addEventListener("change", checkSize);
-    checkSize(mediaQuery);
+    checkSize(mediaQuery); // Инициализируем сразу при запуске
 }
 
-export function myCallback() {
-    const slideHidden = document.querySelectorAll('.slide-hidden');
-    console.log(slideHidden);
-
-
-    
+export function myCallback(shouldInitialize) {
+    if (shouldInitialize) {
+        initializeSwiper();
+    } else {
+        destroySwiper(); 
+    }
 }
